a1cec8344fcfb991397efcb4a979964bfdb0e505
Raymond <raymondurrutia2@yahoo.com>
2025-04-30 09:52:21 -0400

    Create utility function for sending JSON responses

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { sendJSON } from "./utils/sendJSON.js"
 
 const PORT = 8000
 
@@ -7,25 +8,17 @@ const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
   
     if (req.url === '/api' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end(JSON.stringify(destinations))
+        sendJSON(res, 200, destinations)
+        
     } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
         const continent = req.url.split('/').pop()
         const filteredData = destinations.filter((destination) => {
             return destination.continent.toLowerCase() === continent.toLowerCase()
         })
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 200
-        res.end(JSON.stringify(filteredData))
+        sendJSON(res, 300, filteredData)
+
     } else {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 404
-        res.end(JSON.stringify({
-          error: "not found",
-          message: "The requested route does not exist"
-        })
-      )
+        sendJSON(res, 404, { error: "not found", message: "The requested route doesn't exist"})
     }
   })
 
